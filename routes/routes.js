838223e27002aa7be514beae2b4e215e6579557e
Angel Becerra Rojas <alextaco@MacBook-Air-de-Angel.local>
2024-10-10 16:29:06 -0600

    init project

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,13 @@
-function routerApi(app) {}
+const areasRoutes = require('./areaRoutes');
+const departamentosRoutes = require('./departamentoRoutes');
+const empleadosRoutes = require('./empleadoRoutes');
+const encargadosRoutes = require('./encargadoRoutes');
+
+function routerApi(app) {
+  app.use('/areas', areasRoutes);
+  app.use('/departamentos', departamentosRoutes);
+  app.use('empleados', empleadosRoutes);
+  app.use('/encargados', encargadosRoutes);
+}
 
 module.exports = routerApi;
