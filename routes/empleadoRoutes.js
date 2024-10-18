7a1eaa48e2d6d548a78ddd5e20a6cead0a2291a1
angx7 <angelmirocu@gmail.com>
2024-10-18 12:17:12 -0600

    empleados finalizado

--- routes/empleadoRoutes.js
+++ routes/empleadoRoutes.js
@@ -18,8 +18,41 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const nuevoEmpleado = req.body;
   try {
-    service.agregarEmpleado(nuevoEmpleado);
-  } catch (error) {}
+    const createEmpleado = service.agregarEmpleado(nuevoEmpleado);
+    res.status(201).json({ message: 'Empleado creado ', data: createEmpleado });
+  } catch (error) {
+    if (error.message.includes('no existe')) {
+      res.status(404).json({ message: error.message });
+    } else if (error.message.includes('Algo salio')) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'Error interno del servidor' });
+    }
+  }
+});
+
+router.patch('/', (req, res) => {
+  const { id } = req.query;
+  const datosActualizados = req.body;
+  try {
+    const empleadoActualizado = service.modificarEmpleado(
+      id,
+      datosActualizados
+    );
+    res
+      .status(200)
+      .json({ message: 'Empleado actualizado', data: empleadoActualizado });
+  } catch (error) {
+    if (error.message.includes('no encontrado')) {
+      res.status(404).json({ message: error.message });
+    } else if (error.message.includes('cuerpo correcto')) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res
+        .status(500)
+        .json({ message: 'Error interno del servidor', error: error.message });
+    }
+  }
 });
 
 router.delete('/', (req, res) => {
--- services/empleadoService.js
+++ services/empleadoService.js
@@ -1,5 +1,7 @@
 const departamentos = require('../lib/departamentosLib');
 const nombreEmpleado = require('../lib/empleadosLib');
+const departamentosService = require('../services/departamentoService');
+const DepartamentosService = new departamentosService();
 
 function seleccionarDepartamentosAleatorios(departamentos) {
   const seleccionados = [];
@@ -66,58 +68,149 @@ class empleadoService {
       !nuevoEmpleado.departamento_2 ||
       !nuevoEmpleado.departamento_3
     ) {
-      throw new Error('Faltan datos del empleado');
+      throw new Error('Algo salio mal porque faltan datos del empleado');
     }
-    // this.validarDepartamento(nuevoEmpleado.departamento_1, 'departamento_1');
-    // this.validarDepartamento(nuevoEmpleado.departamento_2, 'departamento_2');
-    // this.validarDepartamento(nuevoEmpleado.departamento_3, 'departamento_3');
-    // this.empleados.push(nuevoEmpleado);
 
-    // const departamentosData = departamentosService.getAll();
-    // const departamento1Existe = departamentosData.some(
-    //   (departamento) =>
-    //     departamento.numeroDepartamento === nuevoEmpleado.departamento_1.clave
-    // );
-    // const departamento2Existe = departamentosData.some(
-    //   (departamento) =>
-    //     departamento.numeroDepartamento === nuevoEmpleado.departamento_2.clave
-    // );
-    // const departamento3Existe = departamentosData.some(
-    //   (departamento) =>
-    //     departamento.numeroDepartamento === nuevoEmpleado.departamento_3.clave
-    // );
+    // Validar que los departamentos tengan el cuerpo correcto
+    this.validarDepartamento(nuevoEmpleado.departamento_1, 'departamento_1');
+    this.validarDepartamento(nuevoEmpleado.departamento_2, 'departamento_2');
+    this.validarDepartamento(nuevoEmpleado.departamento_3, 'departamento_3');
 
-    // if (!departamento1Existe || !departamento2Existe || !departamento3Existe) {
-    //   throw new Error('Uno o más departamentos no existen');
-    // }
+    // Verificar la existencia de los departamentos
+    const departamentosData = DepartamentosService.getAll();
+    const departamento1Existe = departamentosData.some(
+      (departamento) =>
+        departamento.numeroDepartamento ===
+          nuevoEmpleado.departamento_1.clave &&
+        departamento.nombre === nuevoEmpleado.departamento_1.nombre
+    );
+    const departamento2Existe = departamentosData.some(
+      (departamento) =>
+        departamento.numeroDepartamento ===
+          nuevoEmpleado.departamento_2.clave &&
+        departamento.nombre === nuevoEmpleado.departamento_2.nombre
+    );
+    const departamento3Existe = departamentosData.some(
+      (departamento) =>
+        departamento.numeroDepartamento ===
+          nuevoEmpleado.departamento_3.clave &&
+        departamento.nombre === nuevoEmpleado.departamento_3.nombre
+    );
 
-    // const newId =
-    //   this.empleados.length > 0
-    //     ? this.empleados[this.empleados.length - 1].numeroEmpleado + 1
-    //     : 1;
+    if (!departamento1Existe || !departamento2Existe || !departamento3Existe) {
+      throw new Error(
+        'Algo salio mal porque uno o más departamentos no existen'
+      );
+    }
 
-    // const empleado = {
-    //   numeroEmpleado: newId,
-    //   nombre: nuevoEmpleado.nombre,
-    //   apellido: nuevoEmpleado.apellido,
-    //   edad: nuevoEmpleado.edad,
-    //   genero: nuevoEmpleado.genero,
-    //   departamento_1: nuevoEmpleado.departamento_1,
-    //   departamento_2: nuevoEmpleado.departamento_2,
-    //   departamento_3: nuevoEmpleado.departamento_3,
-    // };
-    // this.empleados.push(empleado);
+    const newId =
+      this.empleados.length > 0
+        ? this.empleados[this.empleados.length - 1].numeroEmpleado + 1
+        : 1;
+
+    // Crear el nuevo empleado
+    const empleado = {
+      numeroEmpleado: newId,
+      nombre: nuevoEmpleado.nombre,
+      apellido: nuevoEmpleado.apellido,
+      edad: nuevoEmpleado.edad,
+      genero: nuevoEmpleado.genero,
+      departamento_1: nuevoEmpleado.departamento_1,
+      departamento_2: nuevoEmpleado.departamento_2,
+      departamento_3: nuevoEmpleado.departamento_3,
+    };
+
+    // Agregar el nuevo empleado a la lista
+    this.empleados.push(empleado);
+
     return empleado;
   }
 
   validarDepartamento(departamento, nombreDepartamento) {
     if (
       !departamento.nombre ||
-      !departamento.clave ||
+      departamento.clave === undefined ||
       typeof departamento.clave !== 'number'
     ) {
-      throw new Error(`El ${nombreDepartamento} no tiene el cuerpo correcto`);
+      throw new Error(
+        `Algo salio mal porque el ${nombreDepartamento} no tiene el cuerpo correcto`
+      );
     }
+  }
+
+  modificarEmpleado(id, datosActualizados) {
+    const empleadoIndex = this.empleados.findIndex(
+      (item) => item.numeroEmpleado == id
+    );
+
+    if (empleadoIndex === -1) {
+      throw new Error('Empleado no encontrado');
+    }
+
+    const empleadoExistente = this.empleados[empleadoIndex];
+
+    // Validar y actualizar solo los departamentos proporcionados
+    if (datosActualizados.departamento_1) {
+      this.validarDepartamento(
+        datosActualizados.departamento_1,
+        'departamento_1'
+      );
+      const departamento1Existe = departamentosService
+        .getAll()
+        .some(
+          (departamento) =>
+            departamento.numeroDepartamento ===
+              datosActualizados.departamento_1.clave &&
+            departamento.nombre === datosActualizados.departamento_1.nombre
+        );
+      if (!departamento1Existe) {
+        throw new Error('El departamento_1 no existe');
+      }
+      empleadoExistente.departamento_1 = datosActualizados.departamento_1;
+    }
+
+    if (datosActualizados.departamento_2) {
+      this.validarDepartamento(
+        datosActualizados.departamento_2,
+        'departamento_2'
+      );
+      const departamento2Existe = departamentosService
+        .getAll()
+        .some(
+          (departamento) =>
+            departamento.numeroDepartamento ===
+              datosActualizados.departamento_2.clave &&
+            departamento.nombre === datosActualizados.departamento_2.nombre
+        );
+      if (!departamento2Existe) {
+        throw new Error('El departamento_2 no existe');
+      }
+      empleadoExistente.departamento_2 = datosActualizados.departamento_2;
+    }
+
+    if (datosActualizados.departamento_3) {
+      this.validarDepartamento(
+        datosActualizados.departamento_3,
+        'departamento_3'
+      );
+      const departamento3Existe = DepartamentosService.getAll().some(
+        (departamento) =>
+          departamento.numeroDepartamento ===
+            datosActualizados.departamento_3.clave &&
+          departamento.nombre === datosActualizados.departamento_3.nombre
+      );
+      if (!departamento3Existe) {
+        throw new Error('El departamento_3 no existe');
+      }
+      empleadoExistente.departamento_3 = datosActualizados.departamento_3;
+    }
+
+    // Actualizar otros datos del empleado si se proporcionan
+    Object.assign(empleadoExistente, datosActualizados);
+
+    this.empleados[empleadoIndex] = empleadoExistente;
+
+    return empleadoExistente;
   }
 
   delete(id) {
